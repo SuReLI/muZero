cc31d9bc00d4b10d526c29545f14e2bc222f2fd5
mdario <emat.dario@gmail.com>
2024-03-12 14:03:29 +0100

    adapt for mask

--- training.py
+++ training.py
@@ -12,9 +12,10 @@ class LossMuZero():
         self.lv = lv
         self.lp = lp
 
-    def compute_loss(self, 
-            target_reward, # [M*N]
-            pred_reward,   # [M*N]
+    def compute_loss(
+            self, 
+            target_reward, 
+            pred_reward,  
             target_return, 
             pred_return, 
             target_policy, 
@@ -23,14 +24,23 @@ class LossMuZero():
         Compute the loss of the model, given targets and predictions for 
         the next K unrolled steps.
 
+        Args:
+        - target_reward: tensor of target rewards     [M*K*1]
+        - pred_reward  : tensor of predicted rewards  [M*K*1]
+        - target_return: tensor of target returns     [M*K*1]
+        - pred_return  : tensor of predicted returns  [M*K*1]
+        - target_policy: tensor of target policies    [M*K*A] (density)
+        - pred_policy  : tensor of predicted policies [M*K*A] (density)
+
         Loss on the reward is : MSE
         Loss on the return is : MSE
         Loss on the policy is : Cross-entropy
         """
         total_loss = 0
+        K = target_reward.shape[1]
 
         # Compute the loss for each variable
-        for i in range(len(target_reward)):
+        for i in range(K):
             reward_loss = self.lr(pred_reward[i], target_reward[i])
             return_loss = self.lv(pred_return[i], target_return[i])
             policy_loss = self.lp(pred_policy[i], target_policy[i])
@@ -42,21 +52,21 @@ class LossMuZero():
 def compute_predictions(
     observations,
     target_actions,
-    h, g, f
+    h, g, f, 
+    horizon
 ):
     """
     Computes the predictions for each network. Works on batches.
 
     Args:
     - observations: tensor of observations       [M*N*O]
-    - target_actions: tensor of target actions   [M*K]
+    - target_actions: tensor of target actions   [M*K*A]
+    - target_horizon: number of unrolled steps for each trajectory [M*1]
     - h: model for the representation (obs[N-tuple] -> hidden state 's0')
     - g: model for the dynamics (reward, state)
     - f: model for the prediction (policy, value)
+    - horizon: number of unrolled steps (ideal: K=5)
     """
-    M = target_actions.shape[0]
-    K = target_actions.shape[1]
-
     # Initialize the prediction lists
     pred_rewards = []
     pred_returns = []
@@ -65,7 +75,7 @@ def compute_predictions(
     # Predictions for the next K unrolled steps
     prev_state = h(observations) 
 
-    for i in range(K):
+    for i in range(horizon):
         # Predict the reward and the next state
         reward, state = g(prev_state, target_actions[:,i])
 
@@ -81,9 +91,13 @@ def compute_predictions(
         prev_state = state
 
     # Reshape the predictions [M*K]
-    pred_rewards = torch.stack(pred_rewards, dim=1)
-    pred_returns = torch.stack(pred_returns, dim=1)
+    pred_rewards  = torch.stack(pred_rewards, dim=1)
+    pred_returns  = torch.stack(pred_returns, dim=1)
     pred_policies = torch.stack(pred_policies, dim=1)
+
+    # Apply mask over trajectories
+    # 1 * target_horizon + 0 * reste
+    
 
     return pred_rewards, pred_returns, pred_policies
 
@@ -99,6 +113,7 @@ def train_models_one_step(
     optimizer_f,
     criterion,
     h, g, f,
+    horizon,
     verbose=False
 ):
     """
@@ -106,10 +121,10 @@ def train_models_one_step(
     
     Args:
     - observations: tensor of observations       [M*N*O]
-    - target_actions: tensor of target actions   [M*K]
-    - target_rewards: tensor of target rewards   [M*K]
-    - target_returns: tensor of target returns   [M*K]
-    - target_policies: tensor of target policies [M*K*P]
+    - target_actions: tensor of target actions   [M*K*A] (one-hot) (A=nb_actions)
+    - target_rewards: tensor of target rewards   [M*K*1]
+    - target_returns: tensor of target returns   [M*K*1]
+    - target_policies: tensor of target policies [M*K*A] (density)
     - optimizer_h: optimizer for the model h
     - optimizer_g: optimizer for the model g
     - optimizer_f: optimizer for the model h
@@ -117,6 +132,7 @@ def train_models_one_step(
     - h: model for the representation (obs[N-tuple] -> hidden state 's0')
     - g: model for the dynamics (reward, state)
     - f: model for the prediction (policy, value)
+    - horizon: number of unrolled steps (ideal: K=5)s
     - verbose: print the loss at (each) iteration
     """ 
     # Set gradients to zero
@@ -125,7 +141,7 @@ def train_models_one_step(
     optimizer_f.zero_grad()
 
     # Compute the predictions
-    preds = compute_predictions(observations, target_actions, h, g, f)
+    preds = compute_predictions(observations, target_actions, h, g, f, horizon)
     pred_rewards, pred_returns, pred_policies = preds
 
     # Compute the loss
@@ -159,6 +175,7 @@ def valid_models_one_step(
     target_policies,
     criterion,
     h, g, f,
+    horizon,
     verbose=False
 ):
     """
@@ -166,19 +183,20 @@ def valid_models_one_step(
     
     Args:
     - observations: tensor of observations       [M*N*O]
-    - target_actions: tensor of target actions   [M*K]
-    - target_rewards: tensor of target rewards   [M*K]
-    - target_returns: tensor of target returns   [M*K]
-    - target_policies: tensor of target policies [M*K*P]
+    - target_actions: tensor of target actions   [M*K*A] (one-hot) (A=nb_actions)
+    - target_rewards: tensor of target rewards   [M*K*1]
+    - target_returns: tensor of target returns   [M*K*1]
+    - target_policies: tensor of target policies [M*K*A] (density)
     - criterion: the global loss class (containing the 3 losses)
     - h: model for the representation (obs[N-tuple] -> hidden state 's0')
     - g: model for the dynamics (reward, state)
     - f: model for the prediction (policy, value)
+    - horizon: number of unrolled steps (ideal: K=5)
     - verbose: print the loss at (each) iteration
     """ 
     with torch.no_grad():
         # Compute the predictions
-        preds = compute_predictions(observations, target_actions, h, g, f)
+        preds = compute_predictions(observations, target_actions, h, g, f, horizon)
         pred_rewards, pred_returns, pred_policies = preds
 
         # Compute the loss
